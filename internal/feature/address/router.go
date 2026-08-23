94eb75043729064089f65a2c102993b64a9587c1
shadowbone <setyabudiarifin@gmail.com>
2026-08-23 19:57:39 +0700

    [wip] - feature address

--- internal/feature/address/router.go
+++ internal/feature/address/router.go
@@ -20,4 +20,7 @@ func (Router) Register(router fiber.Router, prov provider.Deps) {
 	api := router.Group("/users")
 	api.Get("/:userId/addresses", handler.FindById)
 	api.Post("/:userId/addresses", handler.Store)
+	// PUT    /api/users/:userId/addresses/:id
+	// DELETE /api/users/:userId/addresses/:id
+	// PATCH  /api/users/:userId/addresses/:id/default
 }
